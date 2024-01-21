6ec64b5d4c8de97cabcba502ecf81fa97f185cca
Yuriy L <yuriy.logvinov@gmail.com>
2024-01-21 12:51:43 +0200

    Initial commit

---
+++ Module_1/Hw2/hw2_level1.py
@@ -0,0 +1,26 @@
+fizz = "Fizz"
+buzz = "Buzz"
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+result = []
+
+
+def pure_division(base, element):
+    return element % base == 0
+
+
+def analysis(element):
+    if (pure_division(3, element) and pure_division(5, element)):
+        return fizz + buzz
+    elif (pure_division(3, element) and not pure_division(5, element)):
+        return fizz
+    elif (not pure_division(3, element) and pure_division(5, element)):
+        return buzz
+    else:
+        return element
+
+for element in numbers:
+    result.append(analysis(element))
+
+print("reasult: ", result)
+# link to diagram https://drive.google.com/file/d/13Te0CYxa4-k5TP3SpTdmS0rL2lXsp2y8/view?usp=sharing
+
