282bb8fa501dc80fbec97568f118bde9a541b6ae
Yuriy L <yuriy.logvinov@gmail.com>
2024-01-27 13:56:46 +0200

    redesigned according to recommendations

---
+++ Module1/Home_works/2/level1.py
@@ -0,0 +1,49 @@
+fizz = "Fizz"
+buzz = "Buzz"
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+result = []
+
+
+
+
+def pure_division(element: int, base: int) -> bool:
+    '''
+     return true if parameter `element` is divided to `base` without rest.
+    in other case return false
+    :param element: number that will be divided to `base`
+    :param base: number to which we divide `element`
+    :return: true/false
+    '''
+
+    return element % base == 0
+
+
+def analysis(element: int):
+    '''
+    check if parameter element is can be divided without rest to 3 and/or  5
+    and according to it - return or element or String
+    :param element:  - number that checked
+    :return: integer or string
+    '''
+    if (pure_division(element, 3) and pure_division(element, 5)):
+        return fizz + buzz
+    elif (pure_division(element, 3) and not pure_division(element, 5)):
+        return fizz
+    elif (not pure_division(element, 3) and pure_division(element, 5)):
+        return buzz
+    else:
+        return element
+
+
+def get_result():
+    '''
+    Append to list result processing with analysis() for every element in numbers[]
+    :return: list
+    '''
+
+    for element in numbers:
+        result.append(analysis(element))
+    return result
+
+# print("result: ", getResult())
+# link to diagram https://drive.google.com/file/d/13Te0CYxa4-k5TP3SpTdmS0rL2lXsp2y8/view?usp=sharing
--- Module_2/Home_works/7/main.py
+++ Module_2/Home_works/7/main.py
@@ -1,20 +1,21 @@
-from Module_2.Lesson_4_9.Lesson_2_5.functions import functional_programming as fp
-from Module_1.Hw2 import hw2_level1 as home1
+from Module2.Lesson_4_9.Lesson_2_5.functions import functional_programming as fp
+from Module1.Hw2 import hw2_level1 as home1
 
-choice = int(input("enter your choice: \n1) fizzbuzz\n2) square2d\n"))
-if choice == 1:
-    operation = int(input("enter your operation: \n1) getResult()\n2) analysis for number\n"))
+choice = input("enter your choice: \n1) fizzbuzz\n2) square2d\n")
+
+if choice == "1":
+    operation = input("enter your operation: \n1) getResult()\n2) analysis for number\n")
     print(home1.get_result())
 
-    if operation == 1:
+    if operation == "1":
         print("you selected getResult():", home1.get_result())
-    elif operation == 2:
-        element = int(input("enter element for analysis: "))
+    elif operation == "2":
+        element = input("enter element for analysis: ")
         print(home1.analysis(element))
     else:
         print("wrong operation")
 
-elif choice == 2:
+elif choice == "2":
     operation = int(input("enter your operation: \n1) default triangle()\n2) your own triangle\n"))
 
     if operation == 1:
