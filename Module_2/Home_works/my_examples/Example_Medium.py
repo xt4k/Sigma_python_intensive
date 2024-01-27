ba51e77022eacd452fdb11df7dca1a10da273528
Yuriy L <yuriy.logvinov@gmail.com>
2024-01-27 14:30:15 +0200

    redesigned

--- Module_2/Home_works/my_examples/Example_Medium.py
+++
@@ -1,6 +0,0 @@
-global_name = "This is a global name."
-def update_global():
-    globals()['global_name'] = "Updating a global name from a local scope without the 'global' keyword"
-print("Before calling update_global():", global_name)
-update_global()
-print("After calling update_global():", global_name)
