a426d0219e008681afc6f705328fe091b0183fb2
Yuriy L <yuriy.logvinov@gmail.com>
2024-01-28 14:59:54 +0200

    added how for iterator

---
+++ Learning_Module2/Home_works/Home9/Iterator/Fibonacci.py
@@ -0,0 +1,26 @@
+class Fibonacci:
+    """
+    Class to implement an iterator for fibonacci numbers
+    """
+    def __init__(self, n):
+        self.a = 0
+        self.b = 1
+        self.n = n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.n == 0:
+            raise StopIteration
+
+        a = self.a
+        b = self.b
+        self.a = b
+        self.b = a + b
+        self.n -= 1
+        return a
+
+
+for i in Fibonacci(12):
+    print(i)
